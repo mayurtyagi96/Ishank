8149010d9917b84cdb276b4a1944754a048786dd
mayurtyagi <mayurtyagi@unolo>
2024-08-05 20:28:47 +0530

    Initial Commit

---
+++ Ishank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ishank
+//
+//  Created by Mayur on 05/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ishank/IshankApp.swift
@@ -0,0 +1,17 @@
+//
+//  IshankApp.swift
+//  Ishank
+//
+//  Created by Mayur on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct IshankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
